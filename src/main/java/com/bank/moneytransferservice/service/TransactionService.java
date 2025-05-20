ff254786f0b0e88a622d4eb89ce4508d60f40915
manbarmohamed <manbar.mohammed@gmail.com>
2025-05-20 17:11:54 +0100

    add TransactionService interface with transfer method

---
+++ src/main/java/com/bank/moneytransferservice/service/TransactionService.java
@@ -0,0 +1,8 @@
+package com.bank.moneytransferservice.service;
+
+import com.bank.moneytransferservice.dto.TransferRequestDto;
+import com.bank.moneytransferservice.dto.TransferResponseDto;
+
+public interface TransactionService {
+    TransferResponseDto transfer(TransferRequestDto requestDto);
+}
