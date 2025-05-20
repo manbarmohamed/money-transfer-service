89b3b946958ecfd3d6b32ddde8ea01d6bba11710
manbarmohamed <manbar.mohammed@gmail.com>
2025-05-20 18:14:15 +0100

    add methods to check existence of account by ownerId and accountNumber in AccountRepository

--- src/main/java/com/bank/moneytransferservice/repository/AccountRepository.java
+++ src/main/java/com/bank/moneytransferservice/repository/AccountRepository.java
@@ -9,4 +9,7 @@ import java.util.Optional;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByAccountNumber(String accountNumber);
+    boolean existsByOwnerId(Long userId);
+
+    boolean existsByAccountNumber(String accountNumber);
 }
