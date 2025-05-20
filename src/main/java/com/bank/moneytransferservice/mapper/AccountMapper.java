83d71457df87a1eafe84b5f237d0cf1d338a6503
manbarmohamed <manbar.mohammed@gmail.com>
2025-05-20 18:13:44 +0100

    add userFullName mapping to AccountResponse in AccountMapper

--- src/main/java/com/bank/moneytransferservice/mapper/AccountMapper.java
+++ src/main/java/com/bank/moneytransferservice/mapper/AccountMapper.java
@@ -5,10 +5,12 @@ import com.bank.moneytransferservice.dto.AccountRequest;
 import com.bank.moneytransferservice.dto.AccountResponse;
 import com.bank.moneytransferservice.entity.Account;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
     Account toEntity(AccountRequest request);
     AccountDto toDto(Account account);
+    @Mapping(target = "userFullName", source = "owner.name")
     AccountResponse toResponse(Account account);
 }
